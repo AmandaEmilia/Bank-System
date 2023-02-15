506a7bd493e71c3a3996f8c2909ed66314e8face
Amanda Ström <amandastromiths@hotmail.com>
2023-02-15 09:44:59 +0100

    add test for BankServiceImpl

---
+++ src/test/java/com/bank/service/impl/BankServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.bank.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankServiceImplTest {
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void deposit() {
+    }
+}
