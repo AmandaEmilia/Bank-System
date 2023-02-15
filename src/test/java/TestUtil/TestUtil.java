9cb26a12f2a5cc7e5542346555c8f0e718c98707
Amanda Ström <amandastromiths@hotmail.com>
2023-02-15 13:29:43 +0100

    add test.util

---
+++ src/test/java/TestUtil/TestUtil.java
@@ -0,0 +1,15 @@
+package TestUtil;
+
+import com.bank.BankAccount;
+
+public class TestUtil {
+
+    public static BankAccount getBankAccount(){
+
+        BankAccount bankAccountOne = new BankAccount("Amanda", "1010");
+
+        return bankAccountOne;
+
+    }
+
+}
