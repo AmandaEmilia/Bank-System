008d826b8242ff4fed86844941cd0669973809f2
Amanda Ström <amandastromiths@hotmail.com>
2023-02-15 13:30:15 +0100

    add tests for withdraw and best case deposit

--- src/test/java/com/bank/service/impl/BankServiceImplTest.java
+++ src/test/java/com/bank/service/impl/BankServiceImplTest.java
@@ -1,16 +1,68 @@
 package com.bank.service.impl;
 
+import com.bank.BankAccount;
+import com.bank.BankMenu;
 import org.junit.jupiter.api.Test;
 
+import static TestUtil.TestUtil.getBankAccount;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class BankServiceImplTest {
 
     @Test
-    void withdraw() {
+    void withdrawShouldUpdateBankAccountBalance() {
+        BankServiceImpl bankService = new BankServiceImpl();
+        BankAccount bankAccount = getBankAccount();
+
+        bankAccount.setBalance(500);
+        bankService.withdraw(bankAccount,200);
+
+        assertEquals(bankAccount.getBalance(), 300);
+
+        //bankAccount.setPrevTrans(bankAccount.getPrevTrans());
+
+        //assertEquals(bankAccount.getBalance(),bankAccount.getBalance() - bankAccount.getPrevTrans());
+
+        //assertEquals(bankAccount.getBalance(), 300);
+
+
+        //assertEquals(bankAccount.getBalance(), (bankAccount.setPrevTrans(bankAccount.getBalance() - bankAccount.getPrevTrans()));
+
+
+        //assertEquals(bankAccount.getBalance(), bankAccount.setPrevTrans(bankAccount.getBalance() - bankAccount.getPrevTrans()));
+
+        //bankAccount.getPrevTrans();
+
+
+        //BankMenu bankMenu = new BankMenu(bankService, bankAccount);
+
+
+    }
+
+
+    @Test
+    void withdrawShouldNotBeAccepted(){
+
+        BankServiceImpl bankService = new BankServiceImpl();
+        BankAccount bankAccount = getBankAccount();
+
+        bankAccount.setBalance(100);
+        bankService.withdraw(bankAccount,200);
+
+        assertFalse(bankAccount.getBalance()<bankAccount.getPrevTrans(), "Bank balance insufficient");
+
     }
 
     @Test
-    void deposit() {
+    void depositShouldUpdateBankAccountBalance() {
+        BankServiceImpl bankService = new BankServiceImpl();
+        BankAccount bankAccount = getBankAccount();
+
+        bankAccount.setBalance(400);
+        bankService.deposit(bankAccount,500);
+
+        assertEquals(bankAccount.getBalance(), 900);
+
     }
 }
